a3ec3e5e096ab05f2589b6904a4abd1256cc476a
parsifal <edwinch.gar@gmail.com>
2019-10-13 09:07:12 -0500

    android webview

--- app/src/androidTest/java/com/edwingarcia/semovi05/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/edwingarcia/semovi05/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.edwingarcia.semovi05;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
--- app/src/main/java/com/edwingarcia/semovi05/MainActivity.java
+++ app/src/main/java/com/edwingarcia/semovi05/MainActivity.java
@@ -1,13 +1,22 @@
 package com.edwingarcia.semovi05;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://capacitacionescoco.com");
     }
 }
